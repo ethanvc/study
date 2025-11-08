af3879dd4d5535a3eee50006a2cc5ca0b5395ec6
vc <xht_xht@qq.com>
2025-11-08 13:02:47 +0800

    add code

--- tcpstate/go.mod
+++ tcpstate/go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/elliotchance/orderedmap/v3 v3.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
--- tcpstate/main.go
+++ tcpstate/main.go
@@ -3,15 +3,106 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"net"
+	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/elliotchance/orderedmap/v3"
 )
 
 func main() {
-	AllConnections()
+	targetPosts := orderedmap.NewOrderedMap[int, bool]()
+	for _, target := range os.Args[1:] {
+		port, err := strconv.Atoi(target)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error parsing target port: %v\n", err)
+			return
+		}
+		targetPosts.Set(port, true)
+	}
+	for {
+		connections, err := AllConnections()
+		if err != nil {
+			panic(err)
+		}
+		statistic := getStatistic(connections, targetPosts)
+		printResult(statistic, targetPosts)
+		time.Sleep(2 * time.Second)
+	}
 
+}
+
+var sStatusSLice = []string{
+	"LISTEN", "ESTABLISHED", "SYN_SENT", "CLOSE_WAIT", "TIME_WAIT"}
+
+func mustValidStatus(s string) {
+	for _, status := range sStatusSLice {
+		if s == status {
+			return
+		}
+	}
+	panic("invalid status: " + s)
+}
+
+func printResult(statistic map[int]map[string]int, targetPosts *orderedmap.OrderedMap[int, bool]) {
+	fmt.Println(strings.Repeat("=", 120))
+	fmt.Printf("%s\n", time.Now().Format(time.DateTime))
+	fmt.Printf("%12s", "SVR_PORT")
+	for _, val := range sStatusSLice {
+		fmt.Printf("%12s", val)
+	}
+	fmt.Printf("\n")
+	for port := range targetPosts.AllFromFront() {
+		stat := statistic[port]
+		fmt.Printf("%12d", port)
+		for _, val := range sStatusSLice {
+			fmt.Printf("%12s", getCountOr(stat, val, "-"))
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n\n")
+}
+
+func getCountOr(m map[string]int, key, placeholder string) string {
+	if val, ok := m[key]; ok {
+		return strconv.Itoa(val)
+	} else {
+		return placeholder
+	}
+}
+
+func getStatistic(connections []*ConnectionInfo, targetPosts *orderedmap.OrderedMap[int, bool]) map[int]map[string]int {
+	statistic := make(map[int]map[string]int)
+	for _, connection := range connections {
+		port := getPort(connection.LAddr)
+		if !targetPosts.Has(port) {
+			port = getPort(connection.RAddr)
+		}
+		if !targetPosts.Has(port) {
+			continue
+		}
+		portStatistic := statistic[port]
+		if portStatistic == nil {
+			portStatistic = make(map[string]int)
+			statistic[port] = portStatistic
+		}
+		portStatistic[connection.Status]++
+	}
+	return statistic
+}
+
+func getPort(addr net.Addr) int {
+	switch realAddr := addr.(type) {
+	case *net.TCPAddr:
+		return realAddr.Port
+	default:
+		return 0
+	}
 }
 
 type ConnectionInfo struct {
@@ -37,6 +128,7 @@ func AllConnections() ([]*ConnectionInfo, error) {
 		}
 		laddrStr := parts[4]
 		raddrStr := parts[5]
+		status := parts[6]
 		laddrStr = convertToStandardAddress(laddrStr)
 		laddr, err := net.ResolveTCPAddr("tcp", laddrStr)
 		if err != nil {
@@ -47,9 +139,11 @@ func AllConnections() ([]*ConnectionInfo, error) {
 		if err != nil {
 			return nil, err
 		}
+		mustValidStatus(status)
 		result = append(result, &ConnectionInfo{
-			LAddr: laddr,
-			RAddr: raddr,
+			LAddr:  laddr,
+			RAddr:  raddr,
+			Status: status,
 		})
 	}
 	return result, nil
