d9a1e150d717522cd41dbd12cef5b718cd0801e0
e <xht_xht@qq.com>
2026-01-07 10:55:38 +0800

    add code

--- golangproj/xerr/xerr_test.go
+++ golangproj/xerr/xerr_test.go
@@ -3,6 +3,7 @@ package xerr
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 )
 
@@ -10,5 +11,9 @@ func TestError_Usage(t *testing.T) {
 	// code used for condition check in program code, api should only return predefined codes, please reference
 	// for details:
 	err := New(codes.NotFound, "TxnNotFound")
-	_ = err
+	require.Equal(t, "TxnNotFound", err.GetEvent()[0])
+	err.SetMsg("hello")
+	require.Equal(t, "hello", err.GetMsg())
+	err.SetMsgf("%d", 3)
+	require.Equal(t, "3", err.GetMsg())
 }
