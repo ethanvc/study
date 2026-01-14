e6d92b57ad399c2dc8512bb1e0b65a04475f106c
vc <xht_xht@qq.com>
2026-01-14 21:54:59 +0800

    add code

--- golangproj/logjson/logjson.go
+++ golangproj/logjson/logjson.go
@@ -11,3 +11,11 @@ func NewLogJson() *LogJson {
 func (lj *LogJson) Marshal(v any, opts ...Options) ([]byte, error) {
 	return json.Marshal(v, opts...)
 }
+
+func (lj *LogJson) MarshalAsStr(v any, opts ...Options) (string, error) {
+	buf, err := lj.Marshal(v)
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
--- golangproj/logjson/logjson_test.go
+++ golangproj/logjson/logjson_test.go
@@ -8,12 +8,13 @@ import (
 
 func TestLogJson_Basic(t *testing.T) {
 	type Abc struct {
-		Name string `json:"name" logjson:"md5"`
+		Name   string            `json:"name" logjson:"md5"`
+		Values map[string]string `json:"values" logjson:"map,password:md5,sign_key:md5"`
 	}
 	lj := NewLogJson()
-	buf, err := lj.Marshal(Abc{
+	buf, err := lj.MarshalAsStr(Abc{
 		Name: "test",
 	})
 	require.NoError(t, err)
-	require.Equal(t, `{"Name":""}`, string(buf))
+	require.Equal(t, `{"Name":"md5,4:098f6bcd4621d373cade4e832627b4f6"}`, buf)
 }
