8cd9d0ccb7374e1fda2f51fde3b34bd89801835e
e <xht_xht@qq.com>
2026-03-13 14:31:06 +0800

    add code

--- golangproj/dkit/cmd_grpc.go
+++ golangproj/dkit/cmd_grpc.go
@@ -1,27 +1,81 @@
 package dkit
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// RawCodec is a gRPC codec that passes bytes through without marshaling.
+// Name() returns "proto" so the server treats it as a normal protobuf request.
+type RawCodec struct{}
+
+func (RawCodec) Marshal(v any) ([]byte, error) {
+	switch val := v.(type) {
+	case []byte:
+		return val, nil
+	case string:
+		return []byte(val), nil
+	default:
+		return nil, fmt.Errorf("raw codec: unsupported marshal type %T", v)
+	}
+}
+
+func (RawCodec) Unmarshal(data []byte, v any) error {
+	switch ptr := v.(type) {
+	case *[]byte:
+		*ptr = data
+		return nil
+	default:
+		return fmt.Errorf("raw codec: unsupported unmarshal type %T", v)
+	}
+}
+
+func (RawCodec) Name() string {
+	return "proto"
+}
 
 func AddGrpcCmd(rootCmd *cobra.Command) {
 	cmd := &cobra.Command{
-		Use: "grpc",
+		Use:  "grpc [method]",
+		Args: cobra.ExactArgs(1),
 	}
 	host := cmd.Flags().String("host", "127.0.0.1:8888", "server instance address")
-	body := cmd.Flags().String("body", "", "reqeust content")
+	body := cmd.Flags().String("body", "", "request content")
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 		return SendGrpcRequest(&SendGrpcRequestReq{
-			Host: *host,
-			Body: *body,
+			Host:   *host,
+			Body:   *body,
+			Method: args[0],
 		})
 	}
 	rootCmd.AddCommand(cmd)
 }
 
 type SendGrpcRequestReq struct {
-	Host string
-	Body string
+	Host   string
+	Body   string
+	Method string
 }
 
 func SendGrpcRequest(req *SendGrpcRequestReq) error {
-	return nil
+	cc, err := grpc.NewClient(req.Host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("dial server: %w", err)
+	}
+	defer cc.Close()
+
+	var resp []byte
+	method := "/" + req.Method
+	err = cc.Invoke(context.Background(), method, []byte(req.Body), &resp, grpc.ForceCodec(RawCodec{}))
+	if err != nil {
+		return err
+	}
+
+	_, err = os.Stdout.Write(resp)
+	return err
 }
--- golangproj/go.mod
+++ golangproj/go.mod
@@ -6,17 +6,20 @@ require (
 	github.com/spf13/cobra v1.10.2
 	github.com/stretchr/testify v1.11.1
 	google.golang.org/grpc v1.77.0
+	google.golang.org/protobuf v1.36.10
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
-	google.golang.org/protobuf v1.36.10 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
