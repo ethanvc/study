13bcd62a394cee46b52454bce4f07dc55360aa81
vc <xht_xht@qq.com>
2026-03-26 23:07:46 +0800

    add code

--- golangproj/xobs/xobs_usage_test.go
+++ golangproj/xobs/xobs_usage_test.go
@@ -11,17 +11,6 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-func TestError_Usage(t *testing.T) {
-	// code used for condition check in program code, api should only return predefined codes, please reference
-	// for details:
-	err := New(codes.NotFound, "TxnNotFound")
-	require.Equal(t, "TxnNotFound", err.GetEvent()[0])
-	err.SetMsg("hello")
-	require.Equal(t, "hello", err.GetMsg())
-	err.SetMsgf("%d", 3)
-	require.Equal(t, "3", err.GetMsg())
-}
-
 func Test_Case(t *testing.T) {
 	{
 		// case: return error and let middleware print it in log and do monitor report.
