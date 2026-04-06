b8b5a9f85e12441e38f495bd5d5d3be377f6862f
vc <xht_xht@qq.com>
2026-04-06 23:05:18 +0800

    add code

--- golangproj/logjson/internal/gostd/encoding/json/v2/logjson_arshal.go
+++ golangproj/logjson/internal/gostd/encoding/json/v2/logjson_arshal.go
@@ -14,27 +14,27 @@ func logjsonWrapArshaler(f *structField) {
 	if !f.md5 {
 		return
 	}
+
+	var getBytes func(addressableValue) []byte
+	switch f.typ.Kind() {
+	case reflect.String:
+		getBytes = func(va addressableValue) []byte { return []byte(va.String()) }
+	case reflect.Slice:
+		getBytes = func(va addressableValue) []byte { return va.Bytes() }
+	default:
+		return
+	}
+
 	origFncs := f.fncs
 	wrapped := &arshaler{
 		marshal: func(enc *jsontext.Encoder, va addressableValue, mo *jsonopts.Struct) error {
 			// "len=" (4) + max int64 digits (20) + "," (1) + md5 hex (32) = 57
 			var buf [57]byte
+			data := getBytes(va)
 			b := append(buf[:0], "len="...)
-
-			var h [md5.Size]byte
-			switch va.Kind() {
-			case reflect.String:
-				s := va.String()
-				b = strconv.AppendInt(b, int64(len(s)), 10)
-				h = md5.Sum([]byte(s))
-			case reflect.Slice:
-				data := va.Bytes()
-				b = strconv.AppendInt(b, int64(len(data)), 10)
-				h = md5.Sum(data)
-			default:
-				return origFncs.marshal(enc, va, mo)
-			}
+			b = strconv.AppendInt(b, int64(len(data)), 10)
 			b = append(b, ',')
+			h := md5.Sum(data)
 			b = hex.AppendEncode(b, h[:])
 			return enc.WriteToken(jsontext.String(string(b)))
 		},
